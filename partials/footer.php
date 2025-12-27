e5f1abdf3a5ce638129716b1950bbbac194edac8
DevReza <rezaharjadinata4199@gmail.com>
2025-12-27 17:39:13 +0700

    sss

--- partials/footer.php
+++ partials/footer.php
@@ -1,19 +1,14 @@
-<div class="container">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-      </a>
-      <span class="text-muted">&copy; 2021 Company, Inc</span>
-    </div>
+<footer class="border-top py-3 mt-4 bg-light">
+  <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center gap-2">
+    <small class="mb-0"><b>&copy; 2025 Company, Inc</b></small>
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
-    </ul>
-  </footer>
-</div>
+    <div class="d-flex gap-3">
+      <a href="#" class="text-decoration-none"><i class="fa-brands fa-instagram text-danger"></i></a>
+      <a href="#" class="text-decoration-none"><i class="fa-brands fa-facebook"></i></a>
+      <a href="#" class="text-decoration-none"><i class="fa-brands fa-whatsapp text-success"></i></a>
+    </div>
+  </div>
+</footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </body>
