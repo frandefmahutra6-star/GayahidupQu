36305d9ec8670defc126f3d499ea7ccee77a9943
frandefmahutra6-star <frandefmahutra6@gmail.com>
2025-12-27 17:38:42 +0700

    werty

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-
-</head>
-<body>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-</body>
-</html>
+<?php include 'partials/header.php';?>
+<?php include 'partials/menu.php';?>
+<?php include 'partials/footer.php';?>
---
+++ partials/menu.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-info">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><b>GayahidupQu</b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><b>Tentang Ku</b></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#"><b>Cerita Ku</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><b>Galeri Ku</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><b>Kontak ku</b></a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <a class="btn btn-success"><i class="fa fa-sign-in"></i> Login</a>
+      </form>
+    </div>
+  </div>
+</nav>
