9b82e37b4936979cfd83847032b17b53363fbea9
frandefmahutra6-star <frandefmahutra6@gmail.com>
2026-01-09 17:32:47 +0700

    bebas3

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,19 @@
 <?php include 'partials/header.php';?>
+    <div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">ini halaman dasboard</h3>
+    </div>
 
+    <div class="panel-body text-center">
+        <h1 class="">HALO SEMUA</h1>
+        <p>selamat datang di</p>
+        <hr>
+        <h4>GayahidupQu</h4>
+        <hr>
+    </div>
+
+    <div class="panel-footer">
+
+    </div>
+</div>
 <?php include 'partials/footer.php';?>
