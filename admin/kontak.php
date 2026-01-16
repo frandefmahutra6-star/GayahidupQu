4782598ccf64d3151adc7cfd6c67abc9e1744d78
frandefmahutra6-star <frandefmahutra6@gmail.com>
2026-01-16 16:09:51 +0700

    halaman

--- admin/kontak.php
+++ admin/kontak.php
@@ -15,6 +15,9 @@
     $ig = $data['IG'] ?? '';
     $tw = $data['TW'] ?? '';
     $tt = $data['TT'] ?? '';
+    $alamat=$data['alamat'] ?? '';
+    $whatsapp=$data['whastapp'] ?? '';
+    $email=$data['email'] ?? '';
 
 ?>
 
@@ -33,19 +36,39 @@
                         </div>
                         <div class="form-group">
                             <label for=""><i class="fab fa-instagram"></i> Instagram:</label>
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="ig" value="<?= htmlspecialchars($ig) ?>">
                         </div>
+                        <div class="form-group">
+                            <label for=""><i class="fab fa-whatsapp"></i> whatsapp:</label>
+                            <input type="text" class="form-control" name="whastapp" value="<?= htmlspecialchars($whatsapp) ?>">
+                        </div>
+                        
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for=""><i class="fab fa-twitter"></i> Twitter:</label>
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="tw" value="<?= htmlspecialchars($tw) ?>">
                         </div>
                         <div class="form-group">
                             <label for=""><i class="fab fa-tiktok"></i> Tiktok:</label>
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="tt" value="<?= htmlspecialchars($tt) ?>">
                         </div>
+                        <div class="form-group">
+                            <label for=""> <i class="fa-solid fa-envelope"></i> email:</label>
+                            <input type="text" class="form-control" name="email" value="<?= htmlspecialchars($email) ?>">
+                        </div>
+                        
+                        
+                        
+
+
                     </div>
+                </div>
+                <div class="row">
+                    <div class="form-group">
+                            <label for=""> <i class="fa-solid fa-house"></i> Alamat:</label>
+                            <textarea type="text" class="form-control" name="alamat" value="<?= htmlspecialchars($alamat) ?>"> </textarea>
+                        </div>
                 </div>
             </div>
         </div>
@@ -54,5 +77,6 @@
         </div>
     </form>
 </div>
+
 
 <?php include 'partials/footer.php';?>
--- admin/logika/tambah_medsos.php
+++ admin/logika/tambah_medsos.php
@@ -17,6 +17,57 @@
         }
     }
     
+     $tt = trim($_POST['tt']);
+
+    if($tt !== '') {
+        $check = "SELECT * FROM kontak WHERE jenis_alamat = 'tt'";
+        $hasil_check = mysqli_query($conn, $check);
+
+        if ($hasil_check && mysqli_num_rows($hasil_check) > 0) {
+            $tt_safe = mysqli_real_escape_string($conn, $tt);
+            $query = "UPDATE kontak SET alamat = '$tt_safe' WHERE jenis_alamat = 'TT'";
+            mysqli_query($conn, $query);
+        } else {
+            $query = "INSERT INTO kontak (jenis_alamat, alamat) VALUES ('TT', '$tt')";
+            $result = mysqli_query($conn, $query);
+        }
+    }
+
+    $ig = trim($_POST['ig']);
+
+    if($tt !== '') {
+        $check = "SELECT * FROM kontak WHERE jenis_alamat = 'ig'";
+        $hasil_check = mysqli_query($conn, $check);
+
+        if ($hasil_check && mysqli_num_rows($hasil_check) > 0) {
+            $ig_safe = mysqli_real_escape_string($conn, $ig);
+            $query = "UPDATE kontak SET alamat = '$ig_safe' WHERE jenis_alamat = 'IG'";
+            mysqli_query($conn, $query);
+        } else {
+            $query = "INSERT INTO kontak (jenis_alamat, alamat) VALUES ('IG', '$ig')";
+            $result = mysqli_query($conn, $query);
+        }
+    }
+
+    $tw = trim($_POST['tw']);
+
+    if($tt !== '') {
+        $check = "SELECT * FROM kontak WHERE jenis_alamat = 'tw'";
+        $hasil_check = mysqli_query($conn, $check);
+
+        if ($hasil_check && mysqli_num_rows($hasil_check) > 0) {
+            $tw_safe = mysqli_real_escape_string($conn, $tw);
+            $query = "UPDATE kontak SET alamat = '$tw_safe' WHERE jenis_alamat = 'TW'";
+            mysqli_query($conn, $query);
+        } else {
+            $query = "INSERT INTO kontak (jenis_alamat, alamat) VALUES ('TW', '$ig')";
+            $result = mysqli_query($conn, $query);
+        }
+    }
+
+
+
+    
 
 
     header("Location: ../kontak.php");
